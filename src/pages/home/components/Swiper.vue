4fd148a98741bc7cd372413c43f40906a88f9ecd
yaoyao <574479418@qq.com>
2019-03-14 17:45:25 +0800

    add icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -20,14 +20,16 @@ export default {
         pagination: '.swiper-pagination',
         loop: true
       },
-      swiperList: [{
-        id: '0001 ',
-        imageUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/ad906cbe02849a504c4a4399741013a4.jpg_750x200_8ac64dcc.jpg'
-      },
-      {
-        id: '0002',
-        imageUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/9f73976e40c4ef845cabe0efc0269ebb.jpg_750x200_aab92b7a.jpg'
-      }]
+      swiperList: [
+        {
+          id: '0001',
+          imageUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/9f73976e40c4ef845cabe0efc0269ebb.jpg_750x200_aab92b7a.jpg'
+        },
+        {
+          id: '0002 ',
+          imageUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/ad906cbe02849a504c4a4399741013a4.jpg_750x200_8ac64dcc.jpg'
+        }
+      ]
 
     }
   }
@@ -41,7 +43,7 @@ export default {
         overflow :hidden
         width : 100%
         height : 0
-        padding-bottom 32.67%
+        padding-bottom 26.67%
         .swiper-img
             width:100%
 </style>
