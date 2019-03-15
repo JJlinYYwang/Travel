efc4d93ccb29fb32a3d2063dc8f766bf8d987b97
yaoyao <574479418@qq.com>
2019-03-15 16:27:54 +0800

    index finish

--- src/pages/home/components/weekend.vue
+++ src/pages/home/components/weekend.vue
@@ -4,7 +4,7 @@
     周末去哪儿
     </div>
     <ul>
-      <li class="item  border-bottom"  v-for= "item of weekendList"  :key = "item.id">
+      <li class="item  border-bottom"  v-for= "item of list"  :key = "item.id">
         <div class="item-img-wrapper">
           <img class="item-img" :src="item.imgUrl">
         </div>
@@ -21,28 +21,8 @@
 
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      weekendList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
-        title: '帝都必打卡',
-        desc: '来北京必去的景点非这些地方莫属'
-      },
-      {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg',
-        title: '寻找北京的皇城范儿',
-        desc: '数百年的宫廷庙宇，至今依旧威严霸气'
-      },
-      {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
-        title: '京城周末撒欢',
-        desc: '在帝都过周末，不仅仅是城中游！'
-      }
-      ]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
@@ -58,7 +38,7 @@ export default {
   .item-img-wrapper
     height : 0
     overflow: hidden
-    padding-bottom : 33.9%
+    padding-bottom : 37.09%
     .item-img
       width : 100%
   .item-info
